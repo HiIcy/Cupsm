57696a0163b557179b9fc88701c3bbf4c6a657fc
hiicy <623108389@qq.com>
2022-09-12 16:12:03 +0800

    remove unnessary code

--- src/constants/constants.cpp
+++ src/constants/constants.cpp
@@ -2,7 +2,7 @@
 #include "constants.h"
 
 
-const std::string USER_NAME = "HiIcy";
-const std::string PASSWD = "246A4C29B93EC8D263AAB0DCA152F3079F9061F86D3B3AFE86900A37B1E2C619";
-const std::string BLOG_ADDR = "rnly";
-const std::string URL = "https://rpc.cnblogs.com/metaweblog/rnly";
+const std::string USER_NAME = "";
+const std::string PASSWD = "";
+const std::string BLOG_ADDR = "";
+const std::string URL = "";
